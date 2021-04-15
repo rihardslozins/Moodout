b5cde25dc5e4b55a4332b94d8e2bf4285d316d0a
rihardslozins <rihards.lozins@gmail.com>
2021-04-15 09:13:57 +0300

    Initial Commit

---
+++ Moodout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moodout
+//
+//  Created by Rihards Lozins on 15/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
