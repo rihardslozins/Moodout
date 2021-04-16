e899fe63fd685090c5ac3689bf47d9e4969b9a02
rihardslozins <rihards.lozins@gmail.com>
2021-04-16 13:24:58 +0300

    Design changes

--- Moodout/Views/WelcomeViewController.swift
+++ Moodout/Views/WelcomeViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = false
     }
 
 }
