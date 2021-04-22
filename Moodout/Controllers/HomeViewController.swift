f72cb7b36bd056036d0df67bcd1dbde8db2303f5
rihardslozins <rihards.lozins@gmail.com>
2021-04-22 12:30:15 +0300

    Date formatter added.

--- Moodout/Controllers/HomeViewController.swift
+++ Moodout/Controllers/HomeViewController.swift
@@ -8,9 +8,20 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM yyyy"
+        formatter.timeZone = .current
+        formatter.locale = .current
+        formatter.dateStyle = .medium
+        
+        dateLabel.text = formatter.string(from: date)
         
     }
 }
